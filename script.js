4238aa65a7ffd34a424f96f8eac971b7207aacc6
jonathanhand <hand.jonathan.13@gmail.com>
2019-03-01 14:07:53 -0800

    working recursive function to console

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-document.getElementById("orgData1").innerHTML = "New text!";
+
 //querySelectorAll() to find all items
 const org1_depts = [
     {
@@ -11,7 +11,7 @@ const org1_depts = [
     },
     {
       name: 'finance',
-      children: [],
+      children: [{ name: 'altered', children:[]},],
     },
   ]
   
@@ -31,3 +31,46 @@ const org1_depts = [
       children: [{ name: 'investment', children: [] }],
     },
   ]
+
+
+  function recurs(depts)
+  {
+      for (var i in depts)
+    {
+        console.log(depts[i].name);
+        if(depts[i].children.length > 0)
+        {
+            recurs(depts[i].children);
+        }
+        
+        
+     }
+  }
+  recurs(org2_depts);
+  /*
+  function PrintDepts(depts)
+    {
+        for (var i in depts) 
+        {
+            var parentDept = depts[i];
+            console.log(parentDept.name);
+            var node = document.createElement("p");
+            var textnode = document.createTextNode(parentDept.name);
+            node.appendChild(textnode);
+            document.getElementById("orgData1").appendChild(node);
+            for (var j=0;j<parentDept.children.length;j++)
+            {
+                var node = document.createElement("LI");
+                var textnode = document.createTextNode(parentDept.children[j].name);
+                node.appendChild(textnode);
+                document.getElementById("orgData1").appendChild(node);
+                
+                //document.getElementById("orgData1").innerHTML = parentDept.children[j].name;
+                console.log(parentDept.children[j].name);
+            }
+        }
+     }
+
+PrintDepts(org1_depts);
+//PrintDepts(org2_depts);
+*/
