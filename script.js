f70d3c0092b0925d358003ecc723dc520077bc64
jonathanhand <hand.jonathan.13@gmail.com>
2019-03-05 11:55:39 -0800

    added param to add first element in list without bullet

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const org1_depts = [
     },
     {
       name: 'finance',
-      children: [{ name: 'altered', children:[]},],
+      children: [],
     },
   ]
   
@@ -29,33 +29,36 @@ const org1_depts = [
     },
   ]
 
-
-  function recurs(depts, target)
+  function recurs(depts, target, depth)
   {
+      depth += 1;
       for (var i in depts) //loops through each element in object
     {
 
-        var parentDept = depts[i].name;  //name of the property at index i
-        console.log(parentDept);
-
-        
-        //adding to html with nodes
-        var node = document.createElement("li");
-        var textnode = document.createTextNode(parentDept);
-        node.appendChild(textnode);
-        target.appendChild(node); //appends to current selected target tag
-
+        var deptName = depts[i].name;  //name of the property at index i
+        if (depth === 1)
+        {
+            var ULNode = document.createTextNode(deptName);
+            target.appendChild(ULNode);
+        }
+        else
+        {
+            //adding to html with nodes
+            var node = document.createElement("li");
+            var textnode = document.createTextNode(deptName);
+            node.appendChild(textnode);
+            target.appendChild(node); //appends to current selected target tag
+        }
         if(depts[i].children.length > 0) //when the element has children
         {   
             var childNode = document.createElement('ul');
             var childTextNode = target.appendChild(childNode); //appends new ul to target to make nested list
-
-            recurs(depts[i].children, childNode); //has to pass new target so nests the next children
+            recurs(depts[i].children, childNode, depth); //has to pass new target so nests the next children
         }
         
         
      }
   }
-
-  recurs(org2_depts, document.getElementById("orgUL1")); //added another param for the html div location...
-  recurs(org2_depts, document.getElementById("orgUL2"));   
+  var depth = 0;
+  recurs(org1_depts, document.getElementById("orgUL1"), depth); //added another param for the html div location...
+  recurs(org2_depts, document.getElementById("orgUL2"), depth);   
